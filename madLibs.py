3bbdf27c712d4649d1c55ab6eef0ceaea4985e20
dharaszczuk <d.haraszczuk@vp.pl>
2020-08-16 18:43:04 +0200

    update project madLibs.py

--- madLibs.py
+++ madLibs.py
@@ -1,4 +1,5 @@
 #! python3
+# -*- coding: utf-8 -*-
 # madLibs.py - finds word NOUN, VERB, ADVERB and ADJECTIVE in text and change they to inputted
 
 import re
@@ -6,12 +7,13 @@ import re
 example_text = open("./madLibs.txt")
 read_text = example_text.read()
 print(read_text)    # line for test
-
+print("\n")
 find_word = re.compile(r"NOUN|VERB|ADJECTIVE|ADVERB")
 results_list = find_word.findall(read_text)
 print(results_list)     # line for test
+print("\n")
 
-# Addition new key word
+# Addition new key word and change it in text
 new_word = {}
 num_noun = 0
 num_verb = 0
@@ -20,19 +22,24 @@ num_adj = 0
 for i in range(len(results_list)):
     if results_list[i] == "NOUN":
         new_word["noun{0}".format(num_noun)] = input("Podaj rzeczownik: \n")
+        read_text = read_text.replace(results_list[i], new_word["noun{0}".format(num_noun)], 1)
         num_noun += 1
     elif results_list[i] == "VERB":
         new_word["verb{0}".format(num_verb)] = input("Podaj czasownik: \n")
+        read_text = read_text.replace(results_list[i], new_word["verb{0}".format(num_verb)], 1)
         num_verb += 1
     elif results_list[i] == "ADVERB":
-        new_word["adverb{0}".format(num_adv)] = input("Podaj przys??wek: \n")
+        new_word["adverb{0}".format(num_adv)] = input("Podaj przysłówek: \n")
+        read_text = read_text.replace(results_list[i], new_word["adverb{0}".format(num_adv)], 1)
         num_adv += 1
     elif results_list[i] == "ADJECTIVE":
         new_word["adjective{0}".format(num_adj)] = input("Podaj przymiotnik: \n")
+        read_text = read_text.replace(results_list[i], new_word["adjective{0}".format(num_adj)], 1)
         num_adj += 1
 print(new_word)     # line for test
-example_text.close()
+print("\n")
+print(read_text)    # line for test
+print("\n")
 
-# TODO: Podmiana słów w tekście
 # TODO: Zapisanie tekstu w nowym pliku
-
+example_text.close()
